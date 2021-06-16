587717ddbdde842583484086544f3eb4954d7486
dickymahfudin <dickymahfudin13@gmail.com>
2021-06-16 10:54:32 +0700

    Updates

--- src/middlewares/camelCase.ts
+++ src/middlewares/camelCase.ts
@@ -2,8 +2,12 @@ import camelcaseKeys from 'camelcase-keys';
 import { Request, Response, NextFunction } from 'express';
 
 export const camelCase = (req: Request, res: Response, next: NextFunction) => {
-  req.body = camelcaseKeys(req.body, { deep: true });
-  req.params = camelcaseKeys(req.params);
-  req.query = camelcaseKeys(req.query);
-  return next();
+  try {
+    req.body = camelcaseKeys(req.body, { deep: true });
+    req.params = camelcaseKeys(req.params);
+    req.query = camelcaseKeys(req.query);
+    return next();
+  } catch (error) {
+    return error;
+  }
 };
